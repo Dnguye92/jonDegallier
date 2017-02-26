4551b5a4d1786b406b84cf6cc072513b2ea14296
Dylan <Dnguye92@yahoo.com>
2017-02-25 17:07:37 -0800

    adding JSON for stufor student grades

---
+++ js/grades.js
@@ -0,0 +1,201 @@
+const studentGrades = [
+	{
+		id: 7911,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: 3956
+	},
+	{
+		id: 2557
+	},
+	{
+		id: 9904
+	},
+	{
+		id: 2728
+	},
+	{
+		id: 2330
+	},
+	{
+		id: 1250
+	},
+	{
+		id: 5359
+	},
+	{
+		id: 5852,
+		totalPoints: 41,
+		percent: 100
+	},
+	{
+		id: 4218,
+		totalPoints: 40.5,
+		percent: 99
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+	{
+		id: ,
+		totalPoints: ,
+		percent: 
+	},
+
+
+
+]
