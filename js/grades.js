fbeeb2acaa7264dbbc76fb1786d746d98ceac83b
Dylan <Dnguye92@yahoo.com>
2017-02-25 17:51:12 -0800

    done adding student grades

--- js/grades.js
+++ js/grades.js
@@ -1,29 +1,43 @@
 const studentGrades = [
 	{
 		id: 7911,
-		totalPoints: ,
-		percent: 
+		totalPoints: 41,
+		percent: 100
 	},
 	{
-		id: 3956
+		id: 3956,
+		totalPoints: 41,
+		percent: 100
 	},
 	{
-		id: 2557
+		id: 2557,
+		totalPoints: 41,
+		percent: 100
 	},
 	{
-		id: 9904
+		id: 9904,
+		totalPoints: 41,
+		percent: 100
 	},
 	{
-		id: 2728
+		id: 2728,
+		totalPoints: 41,
+		percent: 100
 	},
 	{
-		id: 2330
+		id: 2330,
+		totalPoints: 41,
+		percent: 100
 	},
 	{
-		id: 1250
+		id: 1250,
+		totalPoints: 41,
+		percent: 100
 	},
 	{
-		id: 5359
+		id: 5359,
+		totalPoints: 41,
+		percent: 100
 	},
 	{
 		id: 5852,
@@ -166,36 +180,208 @@ const studentGrades = [
 		percent: 95
 	},
 	{
-		id: ,
-		totalPoints: ,
-		percent: 
+		id: 0335,
+		totalPoints: 38.5,
+		percent: 94
 	},
 	{
-		id: ,
-		totalPoints: ,
-		percent: 
+		id: 6180,
+		totalPoints: 38,
+		percent: 93
 	},
 	{
-		id: ,
-		totalPoints: ,
-		percent: 
+		id: 4868,
+		totalPoints: 38,
+		percent: 93
 	},
 	{
-		id: ,
-		totalPoints: ,
-		percent: 
+		id: 2510,
+		totalPoints: 37.5,
+		percent: 91
 	},
 	{
-		id: ,
-		totalPoints: ,
-		percent: 
+		id: 1388,
+		totalPoints: 37.5,
+		percent: 91
 	},
 	{
-		id: ,
-		totalPoints: ,
-		percent: 
+		id: 7834,
+		totalPoints: 37,
+		percent: 90
 	},
-
-
-
+	{
+		id: 5732,
+		totalPoints: 37,
+		percent: 90
+	},
+	{
+		id: 4523,
+		totalPoints: 37,
+		percent: 90
+	},
+	{
+		id: 3665,
+		totalPoints: 37,
+		percent: 90
+	},
+	{
+		id: 2687,
+		totalPoints: 36.5,
+		percent: 89
+	},
+	{
+		id: 3477,
+		totalPoints: 36.5,
+		percent: 89
+	},
+	{
+		id: 0966,
+		totalPoints: 36.5,
+		percent: 89
+	},
+	{
+		id: 9991,
+		totalPoints: 36,
+		percent: 88
+	},
+	{
+		id: 0712,
+		totalPoints: 36,
+		percent: 88
+	},
+	{
+		id: 1911,
+		totalPoints: 35.5,
+		percent: 87
+	},
+	{
+		id: 0176,
+		totalPoints: 35.5,
+		percent: 87
+	},
+	{
+		id: 7932,
+		totalPoints: 35.5,
+		percent: 87
+	},
+	{
+		id: 7747,
+		totalPoints: 35.5,
+		percent: 87
+	},
+	{
+		id: 4403,
+		totalPoints: 35,
+		percent: 85
+	},
+	{
+		id: 1644,
+		totalPoints: 35 ,
+		percent: 85
+	},
+	{
+		id: 6725,
+		totalPoints: 34.5,
+		percent: 84
+	},
+	{
+		id: 0689,
+		totalPoints: 34.5,
+		percent: 84
+	},
+	{
+		id: 3367,
+		totalPoints: 34,
+		percent: 83
+	},
+	{
+		id: 4966,
+		totalPoints: 34,
+		percent: 83
+	},
+	{
+		id: 7529,
+		totalPoints: 34,
+		percent: 83
+	},
+	{
+		id: 0925,
+		totalPoints: 32,
+		percent: 78
+	},
+	{
+		id: 6687,
+		totalPoints: 31,
+		percent: 76
+	},
+	{
+		id: 2744,
+		totalPoints: 30.5,
+		percent: 74
+	},
+	{
+		id: 4669,
+		totalPoints: 29.5,
+		percent: 72
+	},
+	{
+		id: 4844,
+		totalPoints: 29,
+		percent: 71
+	},
+	{
+		id: 6859,
+		totalPoints: 28.5,
+		percent: 70
+	},
+	{
+		id: 9038,
+		totalPoints: 27,
+		percent: 66
+	},
+	{
+		id: 3999,
+		totalPoints: 26,
+		percent: 63
+	},
+	{
+		id: 3239,
+		totalPoints: 26,
+		percent: 63
+	},
+	{
+		id: 8947,
+		totalPoints: 25.5,
+		percent: 62
+	},
+	{
+		id: 0171,
+		totalPoints: 25,
+		percent: 61
+	},
+	{
+		id: 6339,
+		totalPoints: 24.5,
+		percent: 60
+	},
+	{
+		id: 6251,
+		totalPoints: 19,
+		percent: 46
+	},
+	{
+		id: 1469,
+		totalPoints: 14,
+		percent: 34
+	},
+	{
+		id: 7708,
+		totalPoints: 5,
+		percent: 12
+	},
+	{
+		id: 6103,
+		totalPoints: 4,
+		percent: 10
+	}
 ]
